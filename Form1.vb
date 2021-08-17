ea9e011e007ba137a9e6374675555c7606720086
Thorben <thorben-renfordt@gmx.de>
2021-08-17 15:14:42 +0200

    test

--- Form1.vb
+++ Form1.vb
@@ -13,7 +13,7 @@ Public Class Form1
 
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        'Test
     End Sub
     Function SolaJahr() As String
         Dim sJahr As String
