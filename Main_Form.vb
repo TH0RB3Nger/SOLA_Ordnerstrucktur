44b773cbc0e4000ca0c6ac9c0d0c73509d291bd8
Thorben <thorben-renfordt@gmx.de>
2022-06-23 20:48:06 +0200

    Test

--- Main_Form.vb
+++ Main_Form.vb
@@ -14,7 +14,7 @@ Public Class Main_Form
     Dim bSolaJahrMan As Boolean
     Dim DL As Char = ";"
 
-
+    'Test
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
